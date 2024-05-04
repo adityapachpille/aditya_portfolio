20b78d0cbed1b089494c86daf8c1d44942d4fe1b
aditya5pille <93114546+aditya5pille@users.noreply.github.com>
2024-05-04 23:00:54 +0530

    first commit

---
+++ src/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["A Front-End Developer!", "A Wordpress Developer!"],
+  typeSpeed: 120,
+  backSpeed: 50,
+  backDelay: 1500,
+  loop: true
+};
+
+var typed = new Typed('#headline', options);
